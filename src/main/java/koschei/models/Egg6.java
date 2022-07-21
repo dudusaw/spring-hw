b2410f5dcb8106cdc924fe74ff28495fc5cfb1f3
nick <d.nikkit2012@gmail.com>
2022-07-21 13:21:54 +0300

    214

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -3,13 +3,11 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Egg6 {
 
     private Needle7 needle7;
 
-    @Autowired
-    public void setNeedle7(Needle7 needle7) {
+    public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
 
